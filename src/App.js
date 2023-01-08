d6256a9d25a9ada6e309ac19d125289aa32c7784
Joshua Fernandes <joshua.mark92@gmail.com>
2023-01-08 15:51:04 -0500

    css changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="h-screen bg-gradient-to-b from-sky-700 to-black">
         <div className="fixed w-full h-full bg-blue flex items-center justify-center flex-col">
-          <div className="bg-black p-12 bg-opacity-75 w-1/2 rounded-lg">
+          <div className="bg-black bg-opacity-75 w-full lg:w-1/2 md:p-12 rounded-lg">
             <div className=" bg-opacity-100">
               <Board></Board>
             </div>
